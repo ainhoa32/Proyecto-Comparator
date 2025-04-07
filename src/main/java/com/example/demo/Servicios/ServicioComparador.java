4fb314156cce9b9053cd386cc313a514152929be
AINHOA NICOLE CONFORME ALARCON <a.conforme@ccoo.es>
2025-04-07 10:41:00 +0200

    Buscador de Mercadona funciona correctamente

---
+++ src/main/java/com/example/demo/Servicios/ServicioComparador.java
@@ -0,0 +1,4 @@
+package com.example.demo.Servicios;
+
+public class ServicioComparador {
+}
--- src/main/java/com/example/demo/controllers/DemoController.java
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -1,4 +1,16 @@
 package com.example.demo.controllers;
 
+import com.example.demo.buscador.supermercados.Mercadona;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DemoController {
+
+    @GetMapping("/prueba")
+    public String prueba(){
+        Mercadona mercadona = new Mercadona();
+        mercadona.comparar();
+        return "prueba";
+    }
 }
