e4064031e90378561863a97514a2f1490fffe72c
AINHOA NICOLE CONFORME ALARCON <a.conforme@ccoo.es>
2025-04-25 13:28:20 +0200

    cambiado cross origin

--- src/main/java/com/proyecto/comparadorProyecto/controllers/ProductosControllerAPIRest.java
+++ src/main/java/com/proyecto/comparadorProyecto/controllers/ProductosControllerAPIRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("productos")
 @AllArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class ProductosControllerAPIRest {
     private final ProductosService productosService;
 
