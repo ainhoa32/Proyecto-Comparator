e590525ac728e1be57fe9e85f15f810f128a816f
ainhoa32 <ainhoa32alarcon@gmail.com>
2025-05-07 16:55:17 +0200

    rama ainhoa retoranada a un punto funcional

--- src/main/java/com/proyecto/comparadorProyecto/buscador/supermercados/Mercadona.java
+++ src/main/java/com/proyecto/comparadorProyecto/buscador/supermercados/Mercadona.java
@@ -104,7 +104,7 @@ public class Mercadona implements Supermercado {
 
     public ProductoDto mapearProducto(Producto producto, int index, List<String> categoriasPrioritarias) {
         PriceInstructions preciosProducto = producto.getPriceInstructions();
-        int prioridad = calculadorPrioridad.calcularSegunCategorias(obtenerCategorias(producto), categoriasPrioritarias);
+        int prioridad = calculadorPrioridad.calcularSegunIndex(index);
 
 
         return ProductoDto.builder()
