10c40dcea239147d36c58c95793b75b6ee30f01d
Kevin_IH <152276338+KevinVk05@users.noreply.github.com>
2025-05-14 12:21:57 +0200

    Base de datos dentro de la aplicación

--- src/main/java/com/proyecto/comparadorProyecto/controllers/UsuarioController.java
+++ src/main/java/com/proyecto/comparadorProyecto/controllers/UsuarioController.java
@@ -1,12 +1,16 @@
 package com.proyecto.comparadorProyecto.controllers;
 
-
 import com.proyecto.comparadorProyecto.dto.LoginRequest;
 import com.proyecto.comparadorProyecto.models.Usuario;
 import com.proyecto.comparadorProyecto.services.UsuarioServicio;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("usuarios")
@@ -14,23 +18,32 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 public class UsuarioController {
 
-
     @Autowired
     private UsuarioServicio usuarioServicio;
 
+    // Logger para la clase
+    private static final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest loginRequest) {
-        System.out.println("Intentando login con:");
-        System.out.println("Nombre: " + loginRequest.getNombre());
-        System.out.println("Contraseña: " + loginRequest.getContrasena());
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        // Loguear intento de login
+        logger.info("Intentando login con Nombre: {}", loginRequest.getNombre());
+
         Usuario usuarioGuardado = usuarioServicio.obtenerUsuarioPorNombre(loginRequest.getNombre());
 
-        if (usuarioGuardado != null &&
-                usuarioServicio.verificarContrasena(loginRequest.getContrasena(), usuarioGuardado.getContrasena())) {
-            return "Login exitoso";
+        // Verificar si el usuario existe
+        if (usuarioGuardado == null) {
+            logger.warn("Usuario no encontrado: {}", loginRequest.getNombre());
+            return new ResponseEntity<>("Nombre de usuario no encontrado", HttpStatus.BAD_REQUEST);
+        }
+
+        // Verificar si la contraseña es correcta
+        if (usuarioServicio.verificarContrasena(loginRequest.getContrasena(), usuarioGuardado.getContrasena())) {
+            logger.info("Login exitoso para el usuario: {}", loginRequest.getNombre());
+            return new ResponseEntity<>("Login exitoso", HttpStatus.OK);
         } else {
-            return "Nombre de usuario o contraseña incorrectos";
+            logger.warn("Contraseña incorrecta para el usuario: {}", loginRequest.getNombre());
+            return new ResponseEntity<>("Contraseña incorrecta", HttpStatus.UNAUTHORIZED);
         }
     }
 }
